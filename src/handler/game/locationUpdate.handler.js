a325dfb8788c3407d8be0198e2326476268cdb3b
이상민 <yihs97@naver.com>
2024-10-31 19:59:20 +0900

    feat 이동 패킷기능 추가

---
+++ src/handler/game/locationUpdate.handler.js
@@ -0,0 +1,27 @@
+import { getGameSession } from '../../sessions/game.session.js';
+
+export const locationUpdateHandler = ({ socket, userId, payload }) => {
+  try {
+    const { x, y } = payload;
+
+    const gameSession = getGameSession();
+    if (!gameSession) {
+      console.error('게임 세션이 존재하지 않습니다.');
+    }
+
+    console.log(gameSession);
+
+    const user = gameSession.getUser(userId);
+    if (!user) {
+      console.error('유저가 존재하지 않습니다.');
+    }
+
+    user.updatePosition(x, y);
+
+    socket.write('');
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export default locationUpdateHandler;
--- src/handler/index.js
+++ src/handler/index.js
@@ -1,10 +1,15 @@
 import { HANDLER_IDS } from '../constants/handlerIds.js';
+import locationUpdateHandler from './game/locationUpdate.handler.js';
 import initialHandler from './user/initial.handler.js';
 
 const handlers = {
   [HANDLER_IDS.INITIAL]: {
     handler: initialHandler,
     protoType: 'initial.InitialPayload',
+  },
+  [HANDLER_IDS.LOCATION_UPDATE]: {
+    handler: locationUpdateHandler,
+    protoType: 'game.LocationUpdatePayload',
   },
 };
 export const getHandlerById = (handlerId) => {
