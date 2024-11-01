aef65d502907bb4d3ac52e72689aa1aa760cdd59
이상민 <yihs97@naver.com>
2024-11-01 14:40:35 +0900

    docs Port 양식 수정

--- src/constants/env.js
+++ src/constants/env.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const HOST = process.env.HOST || '0.0.0.0';
-export const PORT = process.env.PORT || '5555';
+export const PORT = process.env.PORT || 5555;
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
 export const DB_NAME = process.env.DB_NAME || 'USER_DB';
