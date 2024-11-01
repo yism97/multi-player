f6424830a030dd653782aaf8633f1015c59cb6e1
이상민 <yihs97@naver.com>
2024-11-01 13:18:57 +0900

    docs 환경변수 수정

--- src/constants/env.js
+++ src/constants/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const HOST = process.env.HOST || 'localhost';
+export const HOST = process.env.HOST || '0.0.0.0';
 export const PORT = process.env.PORT || '5555';
 export const CLIENT_VERSION = process.env.CLIENT_VERSION || '1.0.0';
 
